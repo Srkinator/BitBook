2867997316fc528020cdab96def923e1cf6b15b8
BIT Student <classroom@bgit.rs>
2017-11-27 17:53:35 +0100

    css for posts done

--- src/components/userPages/feed.js
+++ src/components/userPages/feed.js
@@ -3,6 +3,18 @@ import React, { Component } from "react";
 import DataService from "../../services/dataService";
 import RedirectionService from "../../services/redirectionService";
 
+
+const cardStyle = {
+    width: "85%",
+    height: "95%",
+    padding: "20px",
+    margin: "20px 10px",
+    textAlign: "center",
+    borderRadius: "10%",
+    backgroundColor: "rgba(116, 162, 208, 0.2)",
+    boxShadow: "-12px 11px 34px -1px rgba(44,62,80,0.34)"
+};
+
 class Feed extends Component {
     constructor(props) {
         super(props);
@@ -28,16 +40,18 @@ class Feed extends Component {
 
     render() {
         return (
-            <div>
-                {this.state.posts.map((post) => {
-                    return (
-                        <div key ={post.id} >
-                            <h4>{post.text}</h4>
-                            <p>{post.userDisplayName}</p>
-                            <p>{post.dateCreated}</p>
-                        </div>
-                    );
-                })}
+            <div className="container-fluid">
+                <div className="row">
+                    {this.state.posts.map((post) => {
+                        return (
+                            <div key={post.id} className="col-12" style={cardStyle}>
+                                <h2>{post.userDisplayName}</h2>
+                                <h4>{post.text}</h4>
+                                <h6>{post.dateCreated}</h6>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         );
     }
